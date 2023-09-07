0c752ec11767225df6f54f4c5c8f816fcf04a966
wangdashuai888 <jacob@cz3456.com>
2023-09-07 14:16:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 from cli_version import run_cli_version
 from gui_version import run_gui_version
+import traceback
 
+# def main():
+#     while True:
+#         choice = input("Would you like to use the CLI or GUI version? Enter 'CLI' or 'GUI': ").strip().lower()
+#         if choice == 'cli':
+#             run_cli_version()
+#             break
+#         elif choice == 'gui':
+#             run_gui_version()
+#             break
+#         else:
+#             print("Invalid choice. Please try again.")
+
+#modify the old main into try/except
 def main():
     while True:
-        choice = input("Would you like to use the CLI or GUI version? Enter 'CLI' or 'GUI': ").strip().lower()
-        if choice == 'cli':
-            run_cli_version()
-            break
-        elif choice == 'gui':
-            run_gui_version()
-            break
-        else:
-            print("Invalid choice. Please try again.")
+        try:
+            choice = input("Would you like to use the CLI or GUI version? Enter 'CLI' or 'GUI': ").strip().lower()
+            if choice == 'cli':
+                run_cli_version()
+                break
+            elif choice == 'gui':
+                run_gui_version()
+                break
+            else:
+                print("Invalid choice. Please try again.")
+        except Exception:
+            print("Something went wrong. Please try again.")
+            traceback.print_exc()
 
+
+    
 if __name__ == "__main__":
     main()
